3df230e985e551c3a14d9bbafa7eac1828ae8cd7
Thierry Deo <thierry.deo@booking.com>
2017-03-12 22:51:45 +0100

    Add ExceptionNotification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,4 +21,22 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'bobar.tdb@gmail.com',
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  Rails.application.config.middleware.use ExceptionNotification::Rack, email: {
+    email_prefix: "[TDB - #{Rails.env}] ",
+    sender_address: %("TDB" <bobar.tdb@gmail.com>),
+    exception_recipients: %w(bobar.tdb@gmail.com),
+  }
 end
