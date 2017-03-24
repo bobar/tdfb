dc1fd4d3500cdf997324ad9fb5d8392d0093565e
Matthieu Gendreau <matthieu.gendreau@gmail.com>
2017-03-24 16:00:58 -0400

    Add transactions pagination

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
+gem 'will_paginate', '~> 3.1.0'
 
 group :development, :test do
   gem 'byebug'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -74,4 +74,15 @@ $(document).ready(function() {
       return false;
     }
   });
+
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Please Wait...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
 });
