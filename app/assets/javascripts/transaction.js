ae66045a684c5f425b5191d75950a9edd5c6d8b6
Thierry Deo <thierry.deo@booking.com>
2017-03-31 17:25:02 +0200

    Use icons for cancellation

--- app/assets/javascripts/transaction.js
+++ app/assets/javascripts/transaction.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $('.cancel-transaction').click(function(event) {
     $.ajax({
       data: {
-        id: event.target.getAttribute('t-id'),
-        id2: event.target.getAttribute('t-id2'),
-        date: event.target.getAttribute('t-date'),
-        price: event.target.getAttribute('t-price'),
+        id: event.target.parentElement.getAttribute('t-id'),
+        id2: event.target.parentElement.getAttribute('t-id2'),
+        date: event.target.parentElement.getAttribute('t-date'),
+        price: event.target.parentElement.getAttribute('t-price'),
       },
       method: 'POST',
       dataType: 'script',
