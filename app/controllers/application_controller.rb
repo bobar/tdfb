d4beb144a271ca23b7927ed881ea58eb23852c26
Thierry Deo <thierry.deo@booking.com>
2017-05-21 19:29:57 +0200

    Enforce auth for some actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ require 'digest/md5'
 
 class ApplicationController < ActionController::Base
   AUTH_EXPIRES = 20.seconds
+  FORCE_AUTH = %w(supprimer_tri banque_binet gestion_clopes gestion_admin).freeze
+
   include Chart
 
   before_action do
@@ -67,7 +69,9 @@ class ApplicationController < ActionController::Base
     @bank = Account.find(session[:bank_id])
   end
 
-  def require_admin!(right)
+  def require_admin!(right, force: false)
+    force ||= FORCE_AUTH.include?(right.to_s)
+
     authenticate_with_http_basic do |username, password|
       if session[:expire_at].nil? || session[:expire_at] < Time.current
         session[:expire_at] = Time.current + 24.hours
@@ -77,20 +81,35 @@ class ApplicationController < ActionController::Base
         request_http_basic_authentication
         fail AuthException
       end
+
+      if force
+        # If force = true, we want to enforce the user to type their password, indepently of what happened before,
+        # so we're gonna fail and store the current action in the session so that next time we have a match and we succeed.
+        unless controller_path == session[:force_auth_controller] && action_name == session[:force_auth_action]
+          session[:force_auth_controller] = controller_path
+          session[:force_auth_action] = action_name
+          request_http_basic_authentication
+          fail AuthException
+        end
+      end
+
+      auth = false
       @admin = Admin.joins(:account).includes(:right).find_by(accounts: { trigramme: username })
       if @admin.nil?
         response.header['auth_reason'] = "Unknown admin with trigramme #{username}"
-        false
       elsif Digest::MD5.hexdigest(password) != @admin.passwd
         response.header['auth_reason'] = "Wrong password for #{username}"
-        false
       elsif !@admin.right[right]
         response.header['auth_reason'] = 'You don\'t have the right to do this'
-        false
       else
         session[:expire_at] = Time.current + AUTH_EXPIRES
-        true
+        auth = true
       end
+      if auth
+        session[:force_auth_controller] = nil
+        session[:force_auth_action] = nil
+      end
+      auth
     end || ((session[:expire_at] = Time.current + 24.hours) && request_http_basic_authentication && fail(AuthException))
   end
 end
