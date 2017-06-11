2eeb10055b924a389e02f9bb0d4e18ae411729ae
Thierry Deo <thierry.deo@gmail.com>
2017-06-11 09:39:43 +0200

    Create Bob environment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
   config.dsn = 'https://27660d4e6a794beaacb08a6e987ef257:9139365af07b45ebba4e526a36d97b74@sentry.io/175369'
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w(production development)
+  config.environments = %w(production bob)
 end
